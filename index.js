e63fe370cb99c204b02452bffd27b3423b1ea5c2
Matias Di muro <matiasdimuro22@gmail.com>
2020-08-05 20:04:33 -0300

    cambio de linea CORS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// importamos libreriass 
+// importamos libreriass
 const express  = require('express');
 const mongoose = require('mongoose');
 const cors     = require('cors');
@@ -11,6 +11,9 @@ const port = process.env.PORT        || 3000;
 const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
 
 const app = express();
+// usamos el router
+app.use('/api', router);
+app.use(cors());
 
 // conexion a la base de datos
 mongoose.set('useUnifiedTopology', true);
@@ -22,11 +25,8 @@ mongoose
   })
 .catch(err => console.error(`Connection error ${err}`));
 
-// usamos el router
-app.use('/api', router);
-app.use(cors());
 
 // el server escucha todo
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-}); 
+});
